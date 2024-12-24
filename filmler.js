dcf5588d1f37159bf25d98b9c5c8b58a2b31867c
Vahid Qurbanov <qurbanovvv2006@gmail.com>
2024-12-24 12:34:31 +0400

    first commit

---
+++ filmler.js
@@ -0,0 +1,38 @@
+const movies = [
+    {
+        title: "The Shawshank Redemption",
+        imdbRating: 9.3
+    },
+    {
+        title: "The Dark Knight",
+        imdbRating: 9.0
+    },
+    {
+        title: "Pulp Fiction",
+        imdbRating: 8.9
+    },
+    {
+        title: "Schindler's List",
+        imdbRating: 8.9
+    },
+    {
+        title: "Ayla",
+        imdbRating: 8.5
+    },
+    {
+        title: "Babam ve Oğlum",
+        imdbRating: 8.4
+    },
+    {
+        title: "Kış Uykusu",
+        imdbRating: 8.2
+    },
+    {
+        title: "Vizontele",
+        imdbRating: 8.0
+    },
+    {
+        title: "G.O.R.A.",
+        imdbRating: 7.9
+    }
+];
